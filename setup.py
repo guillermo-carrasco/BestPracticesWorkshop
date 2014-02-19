a13fbb260fd43d241d00d878a2242b65cd3f120b
Guillermo Carrasco <guille.ch.88@gmail.com>
2014-02-19 18:18:10 +0100

    Hej! You can install this material

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='BestPracticesWorkshop',
+      version='1.0',
+      description="Proof of Concept package used in the workshop \"Best " \
+                  "practices on development\" holded in SciLifeLab",
+      author='Guillermo Carrasco',
+      author_email='guillermo.carrasco@scilifelab.se',
+      url='https://github.com/guillermo-carrasco/BestPracticesWorkshop',
+      packages=['BestPracticesWorkshop'],
+)
