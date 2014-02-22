391f245b80b6bf2d7c1be3a49d5d86c563fa0b0e
Guillermo Carrasco <guille.ch.88@gmail.com>
2014-02-22 17:24:27 +0100

    Examples better than exercises, as they are actually examples

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,9 +1,9 @@
 import unittest
 
-from BestPracticesWorkshop import exercises
+from BestPracticesWorkshop import examples
 
-class ExercicesTest(unittest.TestCase):
-    """Test all methods on the exercises module
+class ExamplesTest(unittest.TestCase):
+    """Test all methods on the examples module
     """
 
     def setUp(self):
@@ -20,14 +20,14 @@ class ExercicesTest(unittest.TestCase):
         """Testing sum_list method
         """
         with self.assertRaises(ValueError):
-            exercises.sum_list([1, 2, '3'])
-        self.assertEqual(6, exercises.sum_list([1, 2, 3]))
-        self.assertEqual(4, exercises.sum_list([-1, 2, 3]))
+            examples.sum_list([1, 2, '3'])
+        self.assertEqual(6, examples.sum_list([1, 2, 3]))
+        self.assertEqual(4, examples.sum_list([-1, 2, 3]))
 
     def test_2_odd(self):
         """Testing odd method
         """
         with self.assertRaises(ValueError):
-            exercises.odd('1')
-        self.assertEqual(1, exercises.odd(1))
-        self.assertEqual(3, exercises.odd(2))
+            examples.odd('1')
+        self.assertEqual(1, examples.odd(1))
+        self.assertEqual(3, examples.odd(2))
