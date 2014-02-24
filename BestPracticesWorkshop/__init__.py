b6e0875d31f68083cdb1beae2db001ca616e2e43
Guillermo Carrasco <guille.ch.88@gmail.com>
2014-02-24 17:13:58 +0100

    Ok, everythin works here

--- BestPracticesWorkshop/__init__.py
+++ BestPracticesWorkshop/__init__.py
@@ -1,4 +1,6 @@
 """Module with exercises for the workshop
+
+Author: Guillermo Carrasco, Science For Life Laboratory, Stockholm, Sweden.
 """
 
 REV_COM_TRANSLATION = {'A': 'T', 'C': 'G', 'T': 'A', 'G': 'C'}
