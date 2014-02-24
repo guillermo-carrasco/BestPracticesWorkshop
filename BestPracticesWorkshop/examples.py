d38efa006d9b1ec716526be73310e7d6c5d65df4
Guillermo Carrasco <guille.ch.88@gmail.com>
2014-02-24 14:53:57 +0100

    Sphinx documentation for these methods

--- BestPracticesWorkshop/examples.py
+++ BestPracticesWorkshop/examples.py
@@ -1,8 +1,10 @@
 def sum_list(l):
     """Sum all elements in list
 
-    Params:
-        l -- list to sum
+    :param l: List containing elements to sum
+    :return: The sum of all elements in the list
+    :rtype: int
+    :raises ValueError: If there is any non-integer element on the list
     """
     if not all([isinstance(elem, int) for elem in l]):
         raise ValueError("All elements on the list must be integers!")
@@ -11,11 +13,13 @@ def sum_list(l):
 def odd(n):
     """Return the closest odd number to n
 
-    Paramss:
-        n - An integer
+    :param n: An integer
+    :return: The same integer if it is odd. The next integer otherwise
+    :rtype: int
+    :raises ValueError: If the parameter is not inteer
     """
     if not isinstance(n, int):
-        raise ValueError("The parametermust be an integer!")
+        raise ValueError("The parameter must be an integer!")
     if not n%2:
         return n+1
     return n
