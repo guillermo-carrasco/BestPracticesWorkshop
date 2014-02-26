ed4dfb983dce3f0c336dd314f932ff92140b6c17
Guillermo Carrasco <guille.ch.88@gmail.com>
2014-02-26 22:59:56 +0100

    Added a brief description so people know what the function shoud return

--- tests/test_exercises.py
+++ tests/test_exercises.py
@@ -18,20 +18,30 @@ class ExercisesTest(unittest.TestCase):
 
     def test_1_reverse_complement(self):
         """Testing reverse_complement function
+
+        Return the reverse complement of the given DNA sequence.
         """
         self.assertTrue(False)
 
     def test_2_search(self):
         """Testing search method
+
+        Returns a list containing the indexes of all matches of pattern in text.
+        It also count overlapping matches.
         """
         self.assertTrue(False)
 
     def test_3_extract_kmers(self):
         """Testing extract K-mers function
+
+        Extract all k-mers from the given DNA sequence
         """
         self.assertTrue(False)
 
     def test_4_most_frequent_kmer(self):
         """Testing most_frequent_kmer method
+
+        Returns a list containing the list of most frequent k-mer(s) in DNA. If
+        reverse=True, will also count the reverse complement of the k-mer.
         """
         self.assertTrue(False)
